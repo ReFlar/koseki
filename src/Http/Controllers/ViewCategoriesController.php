ee988f192e5e24aab7ad6c7c01843e5b689a4f15
Shahiem Seymor <s.seymor@live.nl>
2018-09-05 20:21:43 +0200

    Fix missing controller bug

---
+++ src/Http/Controllers/ViewCategoriesController.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of reflar/koseki.
+ *
+ * Copyright (c) ReFlar.
+ *
+ * http://reflar.io
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+ namespace Reflar\Koseki\Http\Controllers;
+
+ use Flarum\Forum\Controller\WebAppController;
+
+ class ViewCategoriesController extends WebAppController
+{
+}
